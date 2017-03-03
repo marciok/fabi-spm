ad63b19f9bd84f5c6acf3e2040f4bc5ea67efa4a
Marcio Klepacz <marcioklepacz@gmail.com>
2017-03-02 23:20:32 -0300

    initial commit

---
+++ Package.swift
@@ -0,0 +1,9 @@
+import PackageDescription
+
+let package = Package(
+    name: "fabi",
+    dependencies: [
+        .Package(url: "https://github.com/marciok/fabi-module.git", majorVersion: 3)
+    ]
+)
+
---
+++ main.swift
@@ -0,0 +1,41 @@
+
+//  main.swift
+//  fabi
+//
+//  Created by Marcio Klepacz on 1/11/17.
+//  Copyright © 2017 Marcio Klepacz. All rights reserved.
+//
+
+import Foundation
+import fabi_module
+
+do {
+    let args = CommandLine.arguments
+    let filePath = args[1]
+    var port: in_port_t? = nil
+    
+    if args[2] == "-p" {
+        port = UInt16(args[3])!
+    }
+    
+    var input = try String(contentsOfFile: filePath)
+    input = preprocess(input)
+    
+    let tokens = tokenizer(input: input)
+    var parser = Parser(tokens: tokens)
+    let handlers = try parser.parseHandler()
+    
+    var server = try HTTPServer(handlers: handlers)
+    if let port = port {
+        try server.start(port: port)
+    } else {
+        try server.start()
+    }
+    
+} catch {
+   print("Error: \(error)")
+}
+
+
+
+
